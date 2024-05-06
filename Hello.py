49fcbe54028a3f80e565b40f1edae95b03957c71
PRATIKCHAUDHARI063 <146616538+PRATIKCHAUDHARI063@users.noreply.github.com>
2024-05-06 05:37:25 +0530

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,3 @@
 print("Hello ! ")
-print("This code Is Writeen in java language :) ")
+print("This code Is Writeen in Python language :) ")
 print("AS the file is git store in git & we are seeing the output at google app engine ) ")
